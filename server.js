91583873581a3652de40beaf01a19ca6e2c094e4
plovaks <polinamasukova77@gmail.com>
2026-04-20 17:26:39 +0300

    changes in database settings

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Настройка базы данных
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
